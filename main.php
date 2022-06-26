13d4acf56d4b8a2dd69d7bc8d69927fe3d4e7fd1
Marco Antonio de Oliveira <marcoadeoli@outlook.com>
2022-06-26 18:56:28 -0300

    Contador de visitas 2

--- main.php
+++ main.php
@@ -1,3 +1,18 @@
+<?php
+$a = 0;
+include 'contador.php';
+if (isset($_COOKIE['counte'])) {
+  $counte = $_COOKIE['counte'] + 1;
+}else{
+$counte = 1;
+$a++; 
+}
+setcookie('counte', "$counte", time()+3700);
+$abre =@fopen("contador.php","w");
+ $ss ='<?php $a='.$a.'; ?>';
+ $escreve =fwrite($abre, $ss);
+ ?>
+
 <!doctype html>
 <html lang="pt-br">
   <head>
@@ -86,7 +101,9 @@
         <small class="text-secondary" id="sugestaorodape">Desenvolvido por: Grupo-052 / PI_II / Univesp / Guarujá / 2022</small>
 
 
-
+<?php 
+echo "<br>$a Pessoas visitaram esse site e você já visitou" .$counte. "vezes";
+?> 
 
 
 
@@ -142,60 +159,7 @@
     <script src="js/jquery.js"></script>
     <script src="js/app.js"></script>
 
-    <script language="JavaScript">
-    function getCookieSlsv (offset) {
-      var endstr = document.cookie.indexOf (";", offset);
-      if (endstr == -1)
-      endstr = document.cookie.length;
-      return unescape(document.cookie.substring(offset, endstr));
-    }
-    function GetCookie (name) {
-      var arg = name + "=";
-      var alen = arg.length;
-      var clen = document.cookie.length;
-      var i = 0;
-      while (i < clen) {
-        var j = i + alen;
-        if (document.cookie.substring(i, j) == arg)
-        return getCookieSlsv (j);
-        i = document.cookie.indexOf(" ", i) + 1;
-        if (i == 0)
-        break;
-      }
-      return null;
-    }
-    function SetCookie (name, value) {
-      var argv = SetCookie.arguments;
-      var argc = SetCookie.arguments.length;
-      var expires = (argc > 2) ? argv[2] : null;
-      var path = (argc > 3) ? argv[3] : null;
-      var domain = (argc > 4) ? argv[4] : null;
-      var secure = (argc > 5) ? argv[5] : false;
-      document.cookie = name + "=" + escape (value) +
-      ((expires == null) ? "" : ("; expires=" + expires.toGMTString()))
-      +
-      ((path == null) ? "" : ("; path=" + path)) +
-      ((domain == null) ? "" : ("; domain=" + domain)) +
-      ((secure == true) ? "; secure" : "");
-    }
-    function DeleteCookie(name) {
-      var exp = new Date();
-      FixCookieDate (exp);
-      exp.setTime (exp.getTime() - 1);
-      var cval = GetCookie (name);
-      if (cval != null)
-      document.cookie = name + "=" + cval + "; expires=" + exp.toGMTString();
-    }
-    var expdate = new Date();
-    var num_visits;
-    expdate.setTime(expdate.getTime() + (365*24*60*60*1000));
-    if (!(num_visits = GetCookie("num_visits")))
-    num_visits = 0;
-    num_visits++;
-    SetCookie("num_visits",num_visits,expdate);
-    document.writeln(num_visits+" vezes!");
-    </script></center></td></tr></table>
-
+    <?php $a=0; ?>
 
 <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
