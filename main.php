b83154a6e43ab6e63ec3a66ec10e1eb1b2c27809
Marco Antonio de Oliveira <marcoadeoli@outlook.com>
2022-06-26 18:41:08 -0300

    Botão 9

--- main.php
+++ main.php
@@ -12,17 +12,6 @@
 
     <title>Sugestões</title>
 
-
-    <script language="javascript"> 
-function setCookie(nome, valor, dias){ 
-diasms = (new Date()) .getTime() + 1000 * 3600 * 24 * dias; 
-dias = new Date(diasms); 
-expires = dias.toGMTString(); 
-document.cookie = escape(nome) + "=" + escape(valor) + "; expires=" + expires; 
-} 
-</script>
-
-
   </head>
   <body>
     <div class="container-fluid main pb-5">
@@ -157,22 +146,64 @@ document.cookie = escape(nome) + "=" + escape(valor) + "; expires=" + expires;
     <script src="js/jquery.js"></script>
     <script src="js/app.js"></script>
 
-    
-    <script language="javascript">
-if (!document.cookie){
-setCookie("cookie", "1", 365);
-document.write("<font face='verdana' size='2'>Suas Visitas : 1</font>");
-} else {
-var cont = document.cookie;
-var dividindo = cont.split("=");
-//document.write(dividindo[1]);
-var numero = parseInt(dividindo[1]);
-var soma = numero + 1;
-document.write("<font face='verdana' size='1'>Suas Visitas : " + soma + "</font>");
-setCookie("cookie", soma, 365);
-}
-</script> 
+    <table border="0" width="200" style="border:4px solid #0098d7; align:center;">
+<tr>
+<td>
+<center>Obrigado por sua visita! Você já visitou esta página
 
+<script language="JavaScript">
+function getCookieSlsv (offset) {
+var endstr = document.cookie.indexOf (";", offset);
+if (endstr == -1)
+endstr = document.cookie.length;
+return unescape(document.cookie.substring(offset, endstr));
+}
+function GetCookie (name) {
+var arg = name + "=";
+var alen = arg.length;
+var clen = document.cookie.length;
+var i = 0;
+while (i < clen) {
+var j = i + alen;
+if (document.cookie.substring(i, j) == arg)
+return getCookieSlsv (j);
+i = document.cookie.indexOf(" ", i) + 1;
+if (i == 0)
+break;
+}
+return null;
+}
+function SetCookie (name, value) {
+var argv = SetCookie.arguments;
+var argc = SetCookie.arguments.length;
+var expires = (argc > 2) ? argv[2] : null;
+var path = (argc > 3) ? argv[3] : null;
+var domain = (argc > 4) ? argv[4] : null;
+var secure = (argc > 5) ? argv[5] : false;
+document.cookie = name + "=" + escape (value) +
+((expires == null) ? "" : ("; expires=" + expires.toGMTString()))
++
+((path == null) ? "" : ("; path=" + path)) +
+((domain == null) ? "" : ("; domain=" + domain)) +
+((secure == true) ? "; secure" : "");
+}
+function DeleteCookie(name) {
+var exp = new Date();
+FixCookieDate (exp);
+exp.setTime (exp.getTime() - 1);
+var cval = GetCookie (name);
+if (cval != null)
+document.cookie = name + "=" + cval + "; expires=" + exp.toGMTString();
+}
+var expdate = new Date();
+var num_visits;
+expdate.setTime(expdate.getTime() + (365*24*60*60*1000));
+if (!(num_visits = GetCookie("num_visits")))
+num_visits = 0;
+num_visits++;
+SetCookie("num_visits",num_visits,expdate);
+document.writeln(num_visits+" vezes!");
+</script></center></td></tr></table>
 
 
 
